fcafb225f04e66212275505653234b10f99a2c62
BrendonHenrique <33273035+BrendonHenrique@users.noreply.github.com>
2018-10-01 18:59:04 -0300

    Update agenda.c
    Agenda funcional , sem ordenação ainda.

--- agenda.c
+++ agenda.c
@@ -6,16 +6,18 @@ typedef struct control{
 	int ContadorDePessoas;
 	int TotalDePessoas;
 	int OpcaoDoMenu;
-
-}control;
+	int i;
+	
+}variaveis;
 
 typedef struct user{
 	char Nome[20];
 	int Num;
-	control controle;
-
+	
 }user;
+
 user *ptrUser;//ptrUser//usuarioAtual
+variaveis *ptrControl; 
 user *aux;//aux//primeiroUsuario
 void *pbuffer;
 
@@ -23,76 +25,106 @@ void *pbuffer;
 void imprimirUm(user *ptrUser){
 
 	//for(aux->controle.ContadorDePessoas = 0 ; aux->controle.ContadorDePessoas < aux->controle.TotalDePessoas ; aux->controle.ContadorDePessoas++ ){
-	printf("\nNome: %s",ptrUser->Nome);
-	printf("\nNumero: %d",ptrUser->Num); 
+	printf("|Nome: %s",ptrUser->Nome);
+	printf("\n|Numero: %d",ptrUser->Num); 
 	ptrUser++;
 	
 }
 
 void imprimirTodos(){
+	
+	printf("\n|=========================[Listar]========================|\n");
 
     user *ptrAtual;
 	
 	ptrAtual = aux;
 
-	for(aux->controle.ContadorDePessoas = 0 ; aux->controle.ContadorDePessoas < aux->controle.TotalDePessoas ; aux->controle.ContadorDePessoas++,ptrAtual++ ){
+	for(ptrControl->ContadorDePessoas = 0 ; ptrControl->ContadorDePessoas < ptrControl->TotalDePessoas ; ptrControl->ContadorDePessoas++,ptrAtual++ ){
 		printf("\nNome: %s",ptrAtual->Nome);
-		printf("\nNumero: %d",ptrAtual->Num); 
+		printf("\nNumero: %d\n",ptrAtual->Num); 
 	}
-
+	printf("|=========================================================|\n");
 }
 
 void inserir(user  *ptrUser){
-	
-	ptrUser->controle.TotalDePessoas++;
-	
-	if(ptrUser->controle.TotalDePessoas > 1){
-	
-		
-		ptrUser = realloc(ptrUser,(ptrUser->controle.TotalDePessoas * sizeof(user)));
-		aux = ptrUser;
 
-		printf("ops aux: %u\n",aux);
+
+	printf("\n|=========================[Inserir]=======================|\n");
+	
+	ptrControl->TotalDePessoas++;
 		
-		for (aux->controle.ContadorDePessoas = 0; aux->controle.ContadorDePessoas < ptrUser->controle.TotalDePessoas; aux->controle.ContadorDePessoas++){
-			ptrUser++;
-		}
-	}else{
-		aux = ptrUser;
-		
-		printf("ops aux: %u\n",aux);
+	pbuffer = realloc(pbuffer,(sizeof(variaveis) + ptrControl->TotalDePessoas * sizeof(user)));
+	
+	ptrUser = pbuffer+sizeof(variaveis);
+	
+	aux = ptrUser;
+	ptrControl = pbuffer;
+	
+	
+	for (ptrControl->ContadorDePessoas = 0; ptrControl->ContadorDePessoas < ptrControl->TotalDePessoas-1; ptrControl->ContadorDePessoas++){
+		ptrUser++;
 	}
-
-	printf("Nome:");
-	scanf("%[^\n]",ptrUser->Nome);
-	printf("Numero:");
+	
+	
+	ptrControl->ContadorDePessoas = 0 ; 
+	
+	printf("|Nome:");
+	scanf("%s",ptrUser->Nome);
+	printf("|Numero:");
 	scanf("%d",&ptrUser->Num);
 	//imprimirUm(ptrUser);
-	ptrUser++;
+	printf("|=========================================================|\n");
 }
-
 
 int main(int argc, char const *argv[])
 {
 	
-	pbuffer = malloc(sizeof(user));
+	pbuffer = malloc(sizeof(variaveis));
+	ptrControl = pbuffer;
 	
 	if(pbuffer == NULL){
 		return -1;
 	}
 
 	ptrUser = pbuffer ;
-	ptrUser->controle.TotalDePessoas = 0 ; 
+	ptrControl->ContadorDePessoas = 0 ; 
+	ptrControl->TotalDePessoas =  0 ; 
 	
 	
-	for (int i = 0; i < 3; i++){
-		inserir(ptrUser);
-		getchar();	
+	for (;;){
+		printf("\n|=========================[Agenda]========================|\n");
+		printf("| 1-Novo contato\n");
+		printf("| 2-Listar contatos\n");
+		printf("| 3-Sair\n");
+		printf("|=========================================================|\n");
+		
+		printf("|Selecione uma das opcoes: ");
+		
+		scanf("%d",&ptrControl->OpcaoDoMenu);
+		
+		switch(ptrControl->OpcaoDoMenu){
+			
+			case 1:
+				
+				inserir(ptrUser);
+				ptrUser = pbuffer;
+				getchar();	
+
+				break;
+			
+			case 2:
+				
+				imprimirTodos();
+				break;
+			
+			case 3:
+				return 0;
+				break;
+			
+		}			
 	}
 
-
 	imprimirTodos();
-
 	
 	return 0;
 }
