5a1dd882a51c18d633ec7a6d6786c7f7cc80b3b2
BrendonHenrique <33273035+BrendonHenrique@users.noreply.github.com>
2018-10-01 15:08:03 -0300

    Update agenda.c

--- agenda.c
+++ agenda.c
@@ -1,64 +1,98 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "estrutura.h"
-
-void imprimir(user *ptrUser){
-
+typedef struct control{
 	
-	for(aux->controle.ContadorDePessoas = 0 ; aux->controle.ContadorDePessoas < aux->controle.TotalDePessoas ; aux->controle.ContadorDePessoas++ ){
-		printf("\nNome: %s",ptrUser->Nome);
-		printf("\nNumero: %d",ptrUser->Num); 
-		ptrUser++;
+	int ContadorDePessoas;
+	int TotalDePessoas;
+	int OpcaoDoMenu;
+
+}control;
+
+typedef struct user{
+	char Nome[20];
+	int Num;
+	control controle;
+
+}user;
+user *ptrUser;//ptrUser//usuarioAtual
+user *aux;//aux//primeiroUsuario
+void *pbuffer;
+
+
+void imprimirUm(user *ptrUser){
+
+	//for(aux->controle.ContadorDePessoas = 0 ; aux->controle.ContadorDePessoas < aux->controle.TotalDePessoas ; aux->controle.ContadorDePessoas++ ){
+	printf("\nNome: %s",ptrUser->Nome);
+	printf("\nNumero: %d",ptrUser->Num); 
+	ptrUser++;
+	
+}
+
+void imprimirTodos(){
+
+    user *ptrAtual;
+	
+	ptrAtual = aux;
+
+	for(aux->controle.ContadorDePessoas = 0 ; aux->controle.ContadorDePessoas < aux->controle.TotalDePessoas ; aux->controle.ContadorDePessoas++,ptrAtual++ ){
+		printf("\nNome: %s",ptrAtual->Nome);
+		printf("\nNumero: %d",ptrAtual->Num); 
 	}
+
 }
 
 void inserir(user  *ptrUser){
-	
 	
 	ptrUser->controle.TotalDePessoas++;
 	
 	if(ptrUser->controle.TotalDePessoas > 1){
 	
+		
 		ptrUser = realloc(ptrUser,(ptrUser->controle.TotalDePessoas * sizeof(user)));
 		aux = ptrUser;
+
+		printf("ops aux: %u\n",aux);
 		
 		for (aux->controle.ContadorDePessoas = 0; aux->controle.ContadorDePessoas < ptrUser->controle.TotalDePessoas; aux->controle.ContadorDePessoas++){
 			ptrUser++;
-		
 		}
-		
 	}else{
 		aux = ptrUser;
+		
+		printf("ops aux: %u\n",aux);
 	}
-	
-		printf("Nome:");
-		scanf("%[^\n]",ptrUser->Nome);
-		printf("Numero:");
-		scanf("%d",&ptrUser->Num);
-		imprimir(aux);
-		
-		ptrUser++;
-		
-}
 
+	printf("Nome:");
+	scanf("%[^\n]",ptrUser->Nome);
+	printf("Numero:");
+	scanf("%d",&ptrUser->Num);
+	//imprimirUm(ptrUser);
+	ptrUser++;
+}
 
 
 int main(int argc, char const *argv[])
 {
-	pbuffer = malloc(sizeof(user));
-	ptrUser = pbuffer ;
 	
+	pbuffer = malloc(sizeof(user));
+	
+	if(pbuffer == NULL){
+		return -1;
+	}
+
+	ptrUser = pbuffer ;
 	ptrUser->controle.TotalDePessoas = 0 ; 
 	
 	
-	for (int i = 0; i < 3; i++)
-	{
-		printf("\nInserindo..!\n");
+	for (int i = 0; i < 3; i++){
 		inserir(ptrUser);
 		getchar();	
-	
 	}
+
+
+	imprimirTodos();
+
 	
 	return 0;
 }
