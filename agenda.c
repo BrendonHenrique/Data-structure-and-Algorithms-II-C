0320179b709c3e937321e6d684ce134bca76ddff
BrendonHenrique <33273035+BrendonHenrique@users.noreply.github.com>
2018-10-01 13:28:55 -0300

    Update agenda.c
    Esta funcional para 2 usuarios , apartir do terceiro o ponteiro perde referencia e imprime lixo.

--- agenda.c
+++ agenda.c
@@ -1,91 +1,66 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-void *pbuffer;
+#include "estrutura.h"
+
+void imprimir(user *ptrUser){
 
 	
-typedef struct controle{
-	
-	int contador;
-	int tamanho;
-	int opcao;
-
-}control;
-
-typedef struct cadastro{
-	char nome[20];
-	int num;
-
-}user;
-
-void inserir(control *ptrControl){
-
-	user *ptrUser;
-	
-	ptrControl->tamanho+=1;
-	
-	pbuffer = realloc(pbuffer,(sizeof(control) + ptrControl->tamanho * sizeof(user)));
-	ptrUser = pbuffer + sizeof(control) + sizeof(user)*ptrControl->tamanho;
-	
-	printf("Nome:");
-	scanf("%[^\n]",ptrUser->nome);
-	
-	printf("\nNumero:");
-	scanf("%d",&ptrUser->num);
-
-}
-
-void imprimir(control *ptrControl){
-
-	user *ptrUser;
-	ptrUser = pbuffer+sizeof(control) ;
-	printf("%d\n", ptrControl->tamanho);
-	for (ptrControl->contador = 0 ; ptrControl->contador < ptrControl->tamanho ; ptrControl->contador++ )
-	{
-		printf("Nome: %s",ptrUser->nome);
-		printf("\nNumero: %d",ptrUser->num);
-		ptrUser = pbuffer + sizeof(control) + ((1+ptrControl->contador)*sizeof(user));
-
-
+	for(aux->controle.ContadorDePessoas = 0 ; aux->controle.ContadorDePessoas < aux->controle.TotalDePessoas ; aux->controle.ContadorDePessoas++ ){
+		printf("\nNome: %s",ptrUser->Nome);
+		printf("\nNumero: %d",ptrUser->Num); 
+		ptrUser++;
 	}
-
 }
+
+void inserir(user  *ptrUser){
+	
+	
+	ptrUser->controle.TotalDePessoas++;
+	
+	if(ptrUser->controle.TotalDePessoas > 1){
+	
+		ptrUser = realloc(ptrUser,(ptrUser->controle.TotalDePessoas * sizeof(user)));
+		aux = ptrUser;
+		
+		for (aux->controle.ContadorDePessoas = 0; aux->controle.ContadorDePessoas < ptrUser->controle.TotalDePessoas; aux->controle.ContadorDePessoas++){
+			ptrUser++;
+		
+		}
+		
+	}else{
+		aux = ptrUser;
+	}
+	
+		printf("Nome:");
+		scanf("%[^\n]",ptrUser->Nome);
+		printf("Numero:");
+		scanf("%d",&ptrUser->Num);
+		imprimir(aux);
+		
+		ptrUser++;
+		
+}
+
 
 
 int main(int argc, char const *argv[])
 {
-	pbuffer = malloc(sizeof(control));
-
-	control *ptrControl;
-
-	ptrControl = pbuffer ;
-
-	ptrControl->contador = 0 ;
-	ptrControl->tamanho = 0; 
-
-	int  *aux;
-	aux = malloc(sizeof(int));
-
-	for (;;){
-			
-			printf("\n(1) Inserir\n(2) Listar\nOpcao ->");
-			scanf("%d",aux);
-			switch (*aux){
-				
-				case 1:
-					printf("Inserindo..!\n");
-					inserir(ptrControl);
-
-				case 2:
-					printf("Listando...\n");
-					imprimir(ptrControl);
-
-			}
-
+	pbuffer = malloc(sizeof(user));
+	ptrUser = pbuffer ;
 	
-		}	
+	ptrUser->controle.TotalDePessoas = 0 ; 
 	
 	
-
+	for (int i = 0; i < 3; i++)
+	{
+		printf("\nInserindo..!\n");
+		inserir(ptrUser);
+		getchar();	
+	
+	}
+	
+	printf("\nListando...\n");
+	
 	return 0;
 }
