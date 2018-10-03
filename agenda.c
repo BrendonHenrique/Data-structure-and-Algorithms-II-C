be8d729de358577d93161ff4defe747068261a55
BrendonHenrique <33273035+BrendonHenrique@users.noreply.github.com>
2018-10-03 01:33:47 -0300

    Update agenda.c
    Agenda Completa , conforme solicitado  com função de busca , listagem , inserção e exclusão.
    Ainda necessário implementar os algorítimos de ordenação , e comentar com mais detalhes as linhas.

--- agenda.c
+++ agenda.c
@@ -1,120 +1,201 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-//struct que pode conter qualquer variável , entretanto as variáveis deverão ser incluidas na memória de pbuffer
-// struct that can contain any variable, however variables must be included in pbuffer memory
 typedef struct control{
 	
 	int ContadorDePessoas; 
+	int ContadorDeExclusao;
 	int TotalDePessoas;
 	int OpcaoDoMenu;
 	int i;
-	
+	char NomeDeBusca[20];
+	char NomeDeExclusao[20];
+	bool NaoEncontrado;
+
 }variaveis;
 
 //Struct para armazenar nome e numero
-//Struct to store name and number
 typedef struct user{
 	char Nome[20];
-	int Num;
+	char Num[10];
 	
 }user;
 
-user *ptrUser;//usuarioAtual//current User
+user *ptrUser;//usuarioAtual
 variaveis *ptrControl; 
-user *aux;//primeiroUsuario //first user 
+user *aux;//primeiroUsuario
 void *pbuffer;
 
 
+// imprime apenas o  usuario passado pelo parametro   
 void imprimirUm(user *ptrUser){
 
 	printf("|Nome: %s",ptrUser->Nome);
-	printf("\n|Numero: %d",ptrUser->Num); 
+	printf("\n|Numero: %s",ptrUser->Num); 
 	
 }
-// function to list all 
+
 void imprimirTodos(){
 	
-	printf("\n|=========================[Listar]========================|\n");
+	printf("\n|====================[Lista De Contatos]==================|");
 
     user *ptrAtual;
 	
 	ptrAtual = aux;
 
+	printf("\n|---------------------------------------------------------|");
+
 	for(ptrControl->ContadorDePessoas = 0 ; ptrControl->ContadorDePessoas < ptrControl->TotalDePessoas ; ptrControl->ContadorDePessoas++,ptrAtual++ ){
-		printf("\nNome: %s",ptrAtual->Nome);
-		printf("\nNumero: %d\n",ptrAtual->Num); 
+		printf("\n|Nome: %s",ptrAtual->Nome);
+		printf("\n|Numero: %s",ptrAtual->Num); 
+		printf("\n|---------------------------------------------------------|");
 	}
-	printf("|=========================================================|\n");
+
+	printf("\n|=========================================================|\n");
 }
-// function to insert  a new name and number
+
 void inserir(user  *ptrUser){
 
 
 	printf("\n|=========================[Inserir]=======================|\n");
 	
-	//ao inserir um usuario aumenta a variável de quantidade de pessoas
-	//at inserting a new user , ptrControl->TotalDePessoas are incressing by one 
+	// ao inserir um usuario aumenta a variável de quantidade de pessoas
 	ptrControl->TotalDePessoas++;
 
 	//realocação de memória em pbuffer , o qual irá receber a primeira posição das variáveis mais a quantidade total de pessoas vezes o tamanho de usuarios		
-	// memory relocation in pbuffer, which will receive the first position of the variables plus the total amount of people times the size of users
 	pbuffer = realloc(pbuffer,(sizeof(variaveis) + ptrControl->TotalDePessoas * sizeof(user)));
 
 	//ptrUser aponta pra pbuffer e pula a primeira posição que é a das variáveis de controle	
-	// ptrUser points to pbuffer and skips the first position that actually is that of control variables
 	ptrUser = pbuffer+sizeof(variaveis);
 	
 	// aux recebe a posição de ptrUser para o ponteiro não perder a referência da primeira posição de usuarios
-	//aux receives the position of ptrUser so the pointer does not lose the reference of the first position of users
 	aux = ptrUser;
 
 	//ptrControl também aponta pra pbuffer porém , ele aponta para a posição a qual tem as variáveis 
 	// mesmo caso que ptrUser porém o ptrUser aponta pra posição a frente das variáveis , as quais o ptrControl manipula
-	// ptrControl also points to pbuffer however, it points to the position which has the variables
-	// even if ptrUser however the ptrUser points to the forward position of the variables, which ptrControl manipulates
 	ptrControl = pbuffer;
 	
 	//Faz um loop em ptrUser  , até que ptrControl->ContadorDePessoas vá de 0 , até ptrControl->TotalDePessoas-1 , ou seja , até o ultimo usuario inserido 
-	// Loops in ptrUser, until ptrControl-> Person Counter goes from 0 to ptrControl-> TotalPeople-1, that is , until the last user inserting
-
 	for(ptrControl->ContadorDePessoas = 0; ptrControl->ContadorDePessoas < ptrControl->TotalDePessoas-1; ptrControl->ContadorDePessoas++,ptrUser++){}
 
 	printf("|Nome:");
 	scanf("%s",ptrUser->Nome);
 	printf("|Numero:");
-	scanf("%d",&ptrUser->Num);
+	scanf("%s",ptrUser->Num);
 	printf("|=========================================================|\n");
 }
+
+void buscar (variaveis *ptrControl){
+	
+	printf("\n|===================[Busca De Contatos]===================|\n");
+	
+	// acessa a primeira posicao dos usuarios
+	ptrControl->NaoEncontrado = true;
+
+	ptrUser = pbuffer + sizeof(variaveis);
+	
+	printf("|Qual nome deseja procurar: ");
+	scanf("%s",ptrControl->NomeDeBusca);
+
+	
+	for(ptrControl->ContadorDePessoas = 0; ptrControl->ContadorDePessoas < ptrControl->TotalDePessoas; ptrControl->ContadorDePessoas++,ptrUser++){
+
+		if((strcmp(ptrUser->Nome,ptrControl->NomeDeBusca))==0){
+		
+			printf("|---------------------------------------------------------|\n");
+			printf("|Usuario encontrado\n|Numero: %s",ptrUser->Num);
+			printf("\n|---------------------------------------------------------|");
+			ptrControl->NaoEncontrado = false;
+		
+			break;
+		}
+	}
+	
+	if(ptrControl->NaoEncontrado == true){
+		printf("|Usuario nao encontrado");
+	}
+	
+	printf("\n|=========================================================|\n");
+}
+ 
+void excluir(variaveis *ptrControl)
+{
+
+	ptrUser = pbuffer + sizeof(variaveis);
+
+	ptrControl = pbuffer;
+
+	variaveis *ptrControlTemp ;
+
+	//printf("trocando %s por %s ",ptrUser->Nome,ptrUser->Nome+sizeof(user));
+	
+	if(ptrControl->TotalDePessoas > 1 )
+	{
+	
+		printf("|Qual nome deseja excluir: ");
+		scanf("%s",ptrControl->NomeDeExclusao);
+
+		for(ptrControl->ContadorDePessoas = 0; ptrControl->ContadorDePessoas < ptrControl->TotalDePessoas; ptrControl->ContadorDePessoas++,ptrUser++)
+		{
+	
+			if((strcmp(ptrUser->Nome,ptrControl->NomeDeExclusao))==0)
+			{
+				for (ptrControl->ContadorDeExclusao = ptrControl->ContadorDePessoas; ptrControl->ContadorDeExclusao < ptrControl->TotalDePessoas ; ptrControl->ContadorDeExclusao++,ptrUser++)
+				{
+					strcpy(ptrUser->Nome,ptrUser->Nome+sizeof(user));
+					strcpy(ptrUser->Num,ptrUser->Num+sizeof(user));
+				}
+
+				ptrControl->TotalDePessoas = ptrControl->TotalDePessoas-1;
+				ptrControl->ContadorDePessoas = ptrControl->ContadorDePessoas-1;		
+ 			}		
+			
+		}
+		printf("\n|---------------------------------------------------------|");
+		printf("\n|Usuario %s excluido!",ptrControl->NomeDeExclusao);
+ 		printf("\n|---------------------------------------------------------|\n");
+	
+	}else{
+
+		ptrControl->TotalDePessoas = 0 ;
+		printf("\n|---------------------------------------------------------|");
+		printf("\n|Usuario %s excluido!",ptrUser->Nome);
+		printf("\n|---------------------------------------------------------|\n");
+	
+	}
+
+}
+
 
 int main(int argc, char const *argv[])
 {
 	
 	//aloca a primeira posição de memória de pbuffer para caber as variaveis que estão na struct variaveis
-	// allocates a first position of pbuffer memory to fit as variables that are in the variable structure
 	pbuffer = malloc(sizeof(variaveis));
 
 	//ptrControl aponta para a posição alocada para as variaveis
-	// ptrControl points to the position allocated to the variables
 	ptrControl = pbuffer;
 	
 	// testa se pbuffer conseguiu alocar
-	// test if pbuffer was able to allocate
 	if(pbuffer == NULL){
 		return -1;
 	}
 	
 	// Inicializa contador de pessoas e total de pessoas
-	// Initialize counter of people and total of people
 	ptrControl->ContadorDePessoas = 0 ; 
 	ptrControl->TotalDePessoas =  0 ; 
 	
 	
 	for (;;){
-		printf("\n|=========================[Agenda]========================|\n");2
+
+		printf("\n|=========================[Agenda]========================|\n");
 		printf("| 1-Novo contato\n");
 		printf("| 2-Listar contatos\n");
-		printf("| 3-Sair\n");
+		printf("| 3-Buscar\n");
+		printf("| 4-Excluir\n");
+		printf("| 5-Sair\n");
 		printf("|=========================================================|\n");
 		
 		printf("|Selecione uma das opcoes: ");
@@ -137,10 +218,18 @@ int main(int argc, char const *argv[])
 				break;
 			
 			case 3:
+
+				buscar(ptrControl);
+				break;
+
+			case 4:
+
+				excluir(ptrControl);
+				break;
+
+			case 5:
 				
 				free(pbuffer);
-				free(ptrUser);
-				free(ptrControl);
 
 				return 0;
 				break;
@@ -148,7 +237,6 @@ int main(int argc, char const *argv[])
 			default:
 				
 				printf("\nOpção incorreta!");
-
 		}			
 	}
 
