a50aa2f4603f3446d79889387993f60e157a5c4d
BrendonHenrique <33273035+BrendonHenrique@users.noreply.github.com>
2018-10-08 17:03:57 -0300

    Update agenda.c
    Adcionado bubble , selection e insertion sort.
    Insertion ainda com erros.

--- agenda.c
+++ agenda.c
@@ -3,16 +3,17 @@
 #include <string.h>
 #include <stdbool.h>
 
-typedef struct control{
-	
+typedef struct control{	
 	int ContadorDePessoas; 
 	int ContadorDeExclusao;
 	int TotalDePessoas;
 	int OpcaoDoMenu;
-	int i;
+	int i,j;
 	char NomeDeBusca[20];
 	char NomeDeExclusao[20];
 	bool NaoEncontrado;
+	char minimo[20],temporario[20];
+	int minimo_id;
 
 }variaveis;
 
@@ -20,14 +21,20 @@ typedef struct control{
 typedef struct user{
 	char Nome[20];
 	char Num[10];
-	
 }user;
 
+//Variaveis globais 
 user *ptrUser;//usuarioAtual
 variaveis *ptrControl; 
 user *aux;//primeiroUsuario
 void *pbuffer;
 
+
+
+
+
+
+/* Funções de controle */
 
 // imprime apenas o  usuario passado pelo parametro   
 void imprimirUm(user *ptrUser){
@@ -169,6 +176,105 @@ void excluir(variaveis *ptrControl)
 }
 
 
+void cls()
+ {
+  	//Código para limpar a tela 
+
+     #ifdef LINUX
+     //código especifico para linux
+     //system ("clear");//poderia ser este mas escolhi este outro pois é mais a cara do C
+     printf("\e[H\e[2J");
+     #elif defined WIN32
+     //código específico para windows
+     system ("cls");
+     #else
+     printf("\e[H\e[2J");
+     #endif
+ 
+ }
+
+
+/*Funcoes de ordenação*/
+void bubblesort (variaveis *PtrControl){
+	ptrControl = pbuffer ;
+	ptrUser = pbuffer + sizeof(variaveis);
+
+	for(ptrControl->i = 0; ptrControl->i < ptrControl->TotalDePessoas - 1 ; ptrControl->i++)
+	{
+
+		for (ptrControl->j = ptrControl->i+1 ; ptrControl->j <  ptrControl->TotalDePessoas ; ptrControl->j++)
+		{
+	
+			if((strcmp(ptrUser[ptrControl->i].Nome,ptrUser[ptrControl->j].Nome))>0)
+			{
+
+				strcpy(ptrControl->NomeDeBusca,ptrUser[ptrControl->i].Nome);
+				strcpy(ptrUser[ptrControl->i].Nome,ptrUser[ptrControl->j].Nome);
+				strcpy(ptrUser[ptrControl->j].Nome,ptrControl->NomeDeBusca);
+			}
+
+		}
+		
+	}
+}
+
+void SelectionSort(variaveis *ptrControl){
+
+	ptrControl = pbuffer ;
+	ptrUser = pbuffer + sizeof(variaveis);
+
+	for(ptrControl->i = 0; ptrControl->i < ptrControl->TotalDePessoas - 1 ; ptrControl->i++)
+	{
+
+		strcpy(ptrControl->minimo , ptrUser[ptrControl->i].Nome);
+
+		for (ptrControl->j = ptrControl->i+1 ; ptrControl->j <  ptrControl->TotalDePessoas ; ptrControl->j++)
+		{
+
+			if((strcmp(ptrUser[ptrControl->j].Nome,ptrControl->minimo))<0)
+			{
+				strcpy(ptrControl->minimo,ptrUser[ptrControl->j].Nome);
+				ptrControl->minimo_id = ptrControl->j;
+
+			}
+		
+		}
+		strcpy(ptrControl->temporario , ptrUser[ptrControl->i].Nome);
+		strcpy(ptrUser[ptrControl->i].Nome , ptrUser[ptrControl->minimo_id].Nome);
+		strcpy(ptrUser[ptrControl->minimo_id].Nome, ptrControl->temporario) ;
+	}
+}
+
+void InsertionSort(variaveis *ptrControl){
+	
+				
+	ptrControl = pbuffer ;
+	ptrUser = pbuffer + sizeof(variaveis);
+
+	for (ptrControl->j = 1; ptrControl->j < ptrControl->TotalDePessoas ; ptrControl->j++)
+	{
+
+		strcpy(ptrControl->temporario,ptrUser[ptrControl->j].Nome);
+
+		ptrControl->i = ptrControl->j - 1 ;
+
+		while(ptrControl->i >= 0 )
+		{	
+
+			if((strcmp(ptrUser[ptrControl->j].Nome,ptrControl->temporario))<=0){
+				break;
+			}
+		
+			strcpy(ptrUser[ptrControl->i + 1].Nome,ptrUser[ptrControl->i].Nome);
+			ptrControl->i = ptrControl->i - 1;
+
+		}
+		strcpy(ptrUser[ptrControl->i+1].Nome , ptrControl->temporario);
+
+	}
+
+}
+
 int main(int argc, char const *argv[])
 {
 	
@@ -195,7 +301,10 @@ int main(int argc, char const *argv[])
 		printf("| 2-Listar contatos\n");
 		printf("| 3-Buscar\n");
 		printf("| 4-Excluir\n");
-		printf("| 5-Sair\n");
+		printf("| 5-BubbleSort\n");
+		printf("| 6-SelectionSort\n");
+		printf("| 7-InsertionSort\n");
+		printf("| 8-Sair\n");
 		printf("|=========================================================|\n");
 		
 		printf("|Selecione uma das opcoes: ");
@@ -206,6 +315,7 @@ int main(int argc, char const *argv[])
 			
 			case 1:
 				
+				cls();
 				inserir(ptrUser);
 				ptrUser = pbuffer;
 				getchar();	
@@ -214,26 +324,57 @@ int main(int argc, char const *argv[])
 			
 			case 2:
 				
+				cls();
 				imprimirTodos();
 				break;
 			
 			case 3:
 
+				cls();
 				buscar(ptrControl);
 				break;
 
 			case 4:
 
+				cls();
 				excluir(ptrControl);
 				break;
 
 			case 5:
 				
+				cls();
+				if(ptrControl->ContadorDePessoas > 0 ){
+				 bubblesort(ptrControl);
+				}else{
+					printf("| Existe apenas 1 usuario impossivel ordenar");
+				}
+
+				break;
+
+			case 6:
+
+				cls();
+				if(ptrControl->ContadorDePessoas > 0 ){
+				SelectionSort(ptrControl);
+				}else{
+					printf("| Existe apenas 1 usuario impossivel ordenar");
+				}
+				
+				break;
+	
+			case 7:
+				cls();
+				InsertionSort(ptrControl);
+				
+				break;
+
+			case 8:
+
 				free(pbuffer);
 
-				return 0;
+				return 1;
 				break;
-			
+					
 			default:
 				
 				printf("\nOpção incorreta!");
